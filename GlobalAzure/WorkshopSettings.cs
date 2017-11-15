79a4a5f85ee854d086edb7f98a60f2b365f220c3
Ashim <ashim.sp@gmail.com>
2017-11-15 13:13:36 +0530

    Create WorkshopSettings.cs

---
+++ GlobalAzure/WorkshopSettings.cs
@@ -0,0 +1,9 @@
+namespace GlobalAzureRacingGame
+{    
+    public class WorkshopSettings
+    {
+        public static bool SendLapData;
+        public static string LapDataConnectionString;
+        public static string LapDataTopicName;
+    }
+}
