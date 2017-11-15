1395d373cd0380788ace33f7946e2c671c7a1a60
Ashim <ashim.sp@gmail.com>
2017-11-15 13:12:44 +0530

    Create LapData.cs

---
+++ GlobalAzure/LapData.cs
@@ -0,0 +1,15 @@
+namespace GlobalAzureRacingGame
+{
+    public class LapData
+    {
+        public string Country { get; set; }
+
+        public string Location { get; set; }
+
+        public string PlayerName { get; set; }
+
+        public string Level { get; set; }
+
+        public string WebLapTime { get; set; }
+    }
+}
